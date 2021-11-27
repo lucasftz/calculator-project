69c8b8853469fa7b2cd1b06916d0e95047220a0d
Lucas Fietkiewicz <lucas.fietkiewicz>
2021-11-27 12:10:49 -0500

    Implement numbers appearing on the calculator display and basic operations

--- static/index.js
+++ static/index.js
@@ -1,1 +1,39 @@
-// This is a comment
+let previousNums = '';
+let currentNums = '';
+let operation;
+
+$('.number').on('click', function(e) {
+    currentNums += e.currentTarget.innerHTML;
+    $('.display-current').html(currentNums);
+});
+
+$('.operation, .equal').on('click', function(e) {
+    if (!!previousNums) {
+        switch (previousNums.slice(-1)) {
+            case '÷':
+                operation = function(a, b) {return a / b};
+            case '×':
+                operation = function(a, b) {return a * b};
+            case '−':
+                operation = function(a, b) {return a - b};
+            case '+':
+                operation = function(a, b) {return a + b};
+        }
+
+        if (e.currentTarget.innerHTML === '=') {
+            currentNums = String(operation(Number(previousNums.slice(0, -1)), Number(currentNums)));
+            previousNums = '';
+        } else {
+            previousNums = String(operation(Number(previousNums.slice(0, -1)), Number(currentNums)));
+            previousNums += e.currentTarget.innerHTML;
+            currentNums = ''
+        }
+        $('.display-previous').html(previousNums);
+        $('.display-current').html(currentNums);
+    } else {
+        previousNums = currentNums + e.currentTarget.innerHTML;
+        currentNums = ''
+        $('.display-previous').html(previousNums);
+        $('.display-current').html(currentNums);
+    }
+});
