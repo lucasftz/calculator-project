c0a4b1830fadc58ab090cef4e30e8431fb80cba2
Lucas Fietkiewicz <lucas.fietkiewicz>
2021-11-27 13:34:03 -0500

    Refactor the code updating the display to remove redundancy

--- static/index.js
+++ static/index.js
@@ -38,14 +38,12 @@ $('.operation, .equal').on('click', function(e) {
             previousNums += e.currentTarget.innerHTML;
             currentNums = ''
         }
-        $('.display-previous').html(previousNums);
-        $('.display-current').html(currentNums);
     } else if (e.currentTarget.innerHTML !== '=' && !!currentNums) {
         previousNums = currentNums + e.currentTarget.innerHTML;
         currentNums = ''
-        $('.display-previous').html(previousNums);
-        $('.display-current').html(currentNums);
     }
+    $('.display-previous').html(previousNums);
+    $('.display-current').html(currentNums);
 });
 
 
